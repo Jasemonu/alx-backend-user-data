1df225ccafa42930e4fbf676f94419e083791604
Jasemonu <jasemonu@gmail.com>
2023-11-07 22:30:59 +0000

    modified

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,13 +5,17 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
-    """A public method"""
+    """manages API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method"""
         return False
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def authorization_header(self, request=None) -> str:
+        """Returns None"""
         return None
 
-
-
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None"""
+        return None
