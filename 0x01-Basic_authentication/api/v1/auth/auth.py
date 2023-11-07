6c779bc7d1cdaeffaa891e9bfa1a0aca085e78ad
Jasemonu <jasemonu@gmail.com>
2023-11-07 23:02:03 +0000

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,14 +10,17 @@ class Auth:
     """manages API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method, checks if authentication is required"""
-        if excluded_paths is None or excluded_paths == [] or path is None:
+        if path is None:
             return True
-        else:
-            if not path.endwith('/'):
-                path = path + '/'
-            if path in excluded_paths:
-                return False
-        return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if not path.endwith('/'):
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        return Truee
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
