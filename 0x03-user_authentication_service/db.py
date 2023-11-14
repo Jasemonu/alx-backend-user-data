81750abc587ff6e44494b96f814b22ec7bbb200d
Jasemonu <jasemonu@gmail.com>
2023-11-14 04:28:51 -0800

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,11 +46,11 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """A method that updates user attributes"""
 
-            user = self.find_user_by(id=user_id)
-            for key, value in kwargs.items():
-                if not hasattr(User, key):
-                    raise ValueError
-                setattr(user, key, value)
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise ValueError
+            setattr(user, key, value)
 
             self._session.commit()
             return None
